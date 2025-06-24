50021922f8ad03edb1b600b118a01030a68d3067
Vitaliy Hlembotskiy <vetal199819@gmail.com>
2025-06-24 20:37:43 +0300

    debug server-test go

--- cmd/server_test.go
+++ cmd/server_test.go
@@ -9,11 +9,11 @@ func TestServerCommandDefined(t *testing.T) {
 		t.Fatal("serverCmd should be defined")
 	}
 	if serverCmd.Use != "server" {
-		t.Error("expected command use 'server', got %s", serverCmd.Use)
+		t.Errorf("expected command use 'server', got %s", serverCmd.Use)
 
 	}
-	 portFlag := serverCmd.Flags().Lookup("port")
-	 if portFlag == nil {
+	portFlag := serverCmd.Flags().Lookup("port")
+	if portFlag == nil {
 		t.Error("expected 'port' flag to be defined")
 	}
 }
