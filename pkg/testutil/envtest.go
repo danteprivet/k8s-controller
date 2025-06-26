5544d129a74a6145f277f99c418b7162b480e39b
Vitaliy Hlembotskiy <vetal199819@gmail.com>
2025-06-26 21:28:13 +0300

    debug apiextensionsv1

--- pkg/testutil/envtest.go
+++ pkg/testutil/envtest.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
