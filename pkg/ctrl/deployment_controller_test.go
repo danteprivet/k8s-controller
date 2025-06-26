f6da8ad6022aa3435219b531837ca0db0d14c4e5
Vitaliy Hlembotskiy <vetal199819@gmail.com>
2025-06-26 22:15:14 +0300

    try debug test

--- pkg/ctrl/deployment_controller_test.go
+++ pkg/ctrl/deployment_controller_test.go
@@ -10,15 +10,19 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func TestDeploymentReconciler_BasicFlow(t *testing.T) {
 	mgr, k8sClient, _, cleanup := testutil.StartTestManager(t)
+	scheme := mgr.GetScheme()
+	err := appsv1.AddToScheme(scheme)
+	require.NoError(t, err)
 	defer cleanup()
 
 	// Register the controller before starting the manager
-	err := AddDeploymentController(mgr)
+	err = AddDeploymentController(mgr)
 	require.NoError(t, err)
 
 	go func() {
