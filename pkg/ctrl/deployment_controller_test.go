b952e049c9dca410e825f4ff628ff47c4a71acb3
Vitaliy Hlembotskiy <vetal199819@gmail.com>
2025-06-26 22:24:20 +0300

    debug

--- pkg/ctrl/deployment_controller_test.go
+++ pkg/ctrl/deployment_controller_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	testutil "github.com/oleksandr-san/k8s-controller/pkg/testutil"
+	testutil "github.com/danteprivet/k8s-controller/pkg/testutil"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
